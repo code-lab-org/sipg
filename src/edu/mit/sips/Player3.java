c12780703e2e552c57f113995d02eee514e89dfd
Paul T. Grogan <pgrogan@stevens.edu>
2013-05-03 21:57:23 +0000

    completely removed old element factory

--- src/edu/mit/sips/Player3.java
+++ src/edu/mit/sips/Player3.java
@@ -4,9 +4,7 @@ import java.util.Arrays;
 
 import javax.swing.SwingUtilities;
 
-import edu.mit.sips.core.City;
 import edu.mit.sips.core.Country;
-import edu.mit.sips.core.agriculture.DefaultAgricultureSystem;
 import edu.mit.sips.gui.DataFrame;
 import edu.mit.sips.sim.Simulator;
 
@@ -23,16 +21,6 @@ public class Player3 {
 		final Country ksa = CountryFactory.createSaudiCountry(
 				Arrays.asList(CityTemplate.RURAL),
 				Arrays.asList(Sector.AGRICULTURE));
-		
-		for(City origin : ksa.getCities()) {
-			for(City destination : ksa.getCities()) {
-				if(!origin.equals(destination)) {
-					DefaultAgricultureSystem.Local las = (DefaultAgricultureSystem.Local) origin.getAgricultureSystem();
-					las.addElement(ElementFactory.createDefaultFoodDistribution(
-							origin.getName(), destination.getName()));
-				}
-			}
-		}
 
 		final Simulator simulator = new Simulator("Agriculture Player", ksa);
 		//simulator.addUpdateListener(new ConsoleLogger());
